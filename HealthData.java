ef25d1d1832ffeae0da62c32fb359f91120ab18d
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2024-04-12 19:04:19 -0230

    Added basic functions for HeathData class

--- HealthData.java
+++ HealthData.java
@@ -1,4 +1,3 @@
-
 public class HealthData {
     private int id;
     private int userId;
@@ -9,4 +8,29 @@ public class HealthData {
     private String date;
 
     // Constructor, getters, and setters
+    public HealthData(int id, int userId, double weight, double height, int steps, int heartRate, String date) {
+        this.id = id;
+        this.userId = id;
+        this.weight = weight;
+        this.height = height;
+        this.steps = steps;
+        this.heartRate = heartRate;
+        this.date = date;
+    }
+
+    public int getId() {return id;}
+    public int getUserId() {return userId;}
+    public double getWeight() {return weight;}
+    public double getHeight() {return height;}
+    public int getSteps() {return steps;}
+    public int getHeartRate() {return heartRate;}
+    public String getDate() {return date;}
+
+    public void setId(int id) {this.id = id;}
+    public void setUserId(int userId) {this.userId = userId;}
+    public void setWeight(double weight) {this.weight = weight;}
+    public void setHeight(double height) {this.height = height;}
+    public void setSteps(int steps) {this.steps = steps;}
+    public void setHeartRate(int heartRate) {this.heartRate = heartRate;}
+    public void setDate(String date) {this.date = date;}
 }
